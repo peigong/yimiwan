5b7565b21a2b964f369fecb5fe48b808f6f6a9a4
周培公 <peigong@foxmail.com>
2019-05-18 18:59:56 +0800

    ok

--- packages/server/config/_wx-conf.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  cgi: 'https://api.weixin.qq.com/cgi-bin',
-  authorize: 'https://open.weixin.qq.com/connect/oauth2/authorize',
-  appId: 'wx34fc357f797989fa',
-  appsecret:  '3148ecabb9ff505aae8a0d50a85ec0b3',
-  token: 'yimiwan2019'
-};
